a56621f422e6c7b667741f7585a775e78e4e19af
Ricardo Cedillo <ricardoxcm@hotmail.com>
2020-01-29 14:25:34 +0100

    Gradient flow analysis for planning distribution network

--- Pendulum_Empowerment_DDPG.py
+++ Pendulum_Empowerment_DDPG.py
@@ -13,6 +13,7 @@ import os
 import pandas as pd
 from gym.wrappers import Monitor
 import math
+from matplotlib.lines import Line2D
 
 i = 0
 while os.path.exists('PendulumRewards/rewards%s.csv' % i):
@@ -112,12 +113,12 @@ class Source(nn.Module):
         x = torch.tanh(self.fc3(x))
         x = torch.tanh(self.fc4(x))
         mu = torch.tanh(self.mu_head(x))
-        sig = F.relu(self.var(x))+0.5
+        sig = torch.tanh((self.var(x)))+0.5
         return mu, sig
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
-        eps = Normal(0, 1)
+        eps = Normal(0, 0.01)
         return mu + eps.sample()*std
 
     def prob(self, act, mean, var):
@@ -125,9 +126,9 @@ class Source(nn.Module):
                torch.exp((-(act - mean) ** 2) / (2 * var ** 2))
 
     def select_action(self, state):
-        mean, var = self.forward(state)
-        act = self.reparameterize(mean, var)
-        prob = self.prob(act, mean, var)
+        mean, variance = self.forward(state)
+        act = self.reparameterize(mean, variance)
+        prob = self.prob(act, mean, variance)
         log_action_prob = torch.log(prob)
         return act, log_action_prob
 
@@ -158,12 +159,12 @@ class Planning(nn.Module):
         x = torch.tanh(self.fc3(x))
         x = torch.tanh(self.fc4(x))
         mu = torch.tanh(self.mu_head(x))
-        sig = F.relu(self.var(x)) + 0.5
+        sig = torch.tanh(self.var(x)) + 0.5
         return mu, sig
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
-        eps = Normal(0, 1)
+        eps = Normal(0, 0.01)
         return mu + eps.sample()*std
 
     def prob(self, act, mean, var):
@@ -176,6 +177,49 @@ class Planning(nn.Module):
         log_action_prob = torch.log(prob)
         return act, log_action_prob
 
+
+def plot_grad_flow(named_parameters):
+    '''Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+
+    Usage: Plug this function in Trainer class after loss.backwards() as
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
+    ave_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.show()
+'''    ave_grads = []
+    max_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if (p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+            max_grads.append(p.grad.abs().max())
+    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads) + 1, lw=2, color="k")
+    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend([Line2D([0], [0], color="c", lw=4),
+                Line2D([0], [0], color="b", lw=4),
+                Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])'''
 
 class ActorNet(nn.Module):
     def __init__(self, n_actions, n_states):
@@ -309,6 +353,7 @@ while epoch <= 800:
     training_records.append(TrainingRecord(epoch, running_reward, empowerment.item()))
     empowerment = -(1/M)*empowerment
     empowerment.backward(retain_graph=True)
+    plot_grad_flow(planning_network.named_parameters())
     source_optimizer.step()
     planning_optimizer.step()
     print('Epoch {}\tAverage score: {:.2f}\tAverage Q: {:.2f}\tEmpowerment: {:.2f}'.format(
